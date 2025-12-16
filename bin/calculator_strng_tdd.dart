486e910012f2180a2d175a4200af3ca7864fbbea
Sandeep Devrari <107464611+sandeepdevrarimrcc@users.noreply.github.com>
2025-12-17 00:27:54 +0530

    pass
    Signed-off-by: Sandeep Devrari <107464611+sandeepdevrarimrcc@users.noreply.github.com>

--- bin/calculator_strng_tdd.dart
+++ bin/calculator_strng_tdd.dart
@@ -5,6 +5,7 @@ void main(List<String> arguments) {
   test("input '1' output 1", ()=> expect(add('1'), 1));
   test("input '2' output 2", ()=> expect(add('2'), 2));
   test("input '3' output 3", ()=> expect(add('3'), 3));
+  test("input '10' output 10", ()=> expect(add('10'), 10));
 }
 
 int add(String numbers){
