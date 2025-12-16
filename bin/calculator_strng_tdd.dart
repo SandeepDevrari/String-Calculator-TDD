f69ca96625bc6ee5281f3f1e8c5b916da232b849
Sandeep Devrari <107464611+sandeepdevrarimrcc@users.noreply.github.com>
2025-12-17 00:49:05 +0530

    pass
    Signed-off-by: Sandeep Devrari <107464611+sandeepdevrarimrcc@users.noreply.github.com>

--- bin/calculator_strng_tdd.dart
+++ bin/calculator_strng_tdd.dart
@@ -16,6 +16,7 @@ void main(List<String> arguments) {
   test("input '1\n\n2,3\n\n\n\n\n4\n5\n6,,,,,,7\n\n\n\n10,,,,' output 38", ()=> expect(add('1\n\n2,3\n\n\n\n\n4\n5\n6,,,,,,7\n\n\n\n10,,,,'), 38));
   test("input '//;\n1;2' output 3", ()=> expect(add('//;\n1;2'), 3));
   test("input '//;\n1;2;3;4;5\n6\n7\n8' output 36", ()=> expect(add('//;\n1;2;3;4;5\n6\n7\n8'), 36));
+  test("input '//-\n1-2-3-4-5\n6\n7\n8' output 36", ()=> expect(add('//-\n1-2-3-4-5\n6\n7\n8'), 36));
 }
 
 int add(String numbers){
