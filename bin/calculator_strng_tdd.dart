37ca06f689de3e3ddc7d3a97747e38782a33114a
Sandeep Devrari <107464611+sandeepdevrarimrcc@users.noreply.github.com>
2025-12-17 00:24:44 +0530

    2nd test failed
    Signed-off-by: Sandeep Devrari <107464611+sandeepdevrarimrcc@users.noreply.github.com>

--- bin/calculator_strng_tdd.dart
+++ bin/calculator_strng_tdd.dart
@@ -2,6 +2,7 @@ import 'package:test/test.dart';
 
 void main(List<String> arguments) {
   test("input '' output 0", ()=> expect(add(''), 0));
+  test("input '1' output 1", ()=> expect(add('1'), 1));
 }
 
 int add(String numbers){
